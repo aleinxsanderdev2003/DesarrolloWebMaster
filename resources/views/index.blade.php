37a6c76efea9b527276b7449ddd32f67a06952a7
aleinxsanderdev2003 <alexander_03c@outlook.es>
2023-11-07 23:20:03 -0500

    Chupame la ganpi

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,45 +1,7 @@
 @extends('layout.app')
 @section('content')
-<style>
-      /* Estilos para las imágenes y textos */
-      .carousel-item img {
-            width: 100%;
-            max-height: 400px; /* Altura máxima deseada */
-            object-fit: cover; /* Escala y recorta la imagen */
-        }
-        .carousel-caption {
-            background: rgba(0, 0, 0, 0.7); /* Fondo oscuro con opacidad */
-            border-radius: 5px;
-            text-align: center;
-            padding: 10px;
-        }
-        .carousel-caption h3, .carousel-caption p {
-            font-size: 24px; /* Tamaño del texto */
-            color: white;
-        }
-        /* Estilo para los botones de navegación */
-        .carousel-control-prev, .carousel-control-next {
-            background: rgba(0, 0, 0, 0.7);
-            border-radius: 50%;
-            width: 50px;
-            height: 50px;
-            top: 50%;
-            transform: translateY(-50%);
-        }
-        .carousel-control-prev-icon, .carousel-control-next-icon {
-            background-color: transparent;
-        }
-        .carousel-control-prev-icon::before, .carousel-control-next-icon::before {
-            content: "";
-        }
-        /* Estilo para los controles de navegación en dispositivos móviles */
-        @media (max-width: 768px) {
-            .carousel-control-prev, .carousel-control-next {
-                width: 30px;
-                height: 30px;
-            }
-        }
-</style>  <div id="promocionesCarousel" class="carousel slide" data-bs-ride="carousel">
+
+<div id="promocionesCarousel" class="carousel slide" data-bs-ride="carousel">
     <!-- Slides del carrusel -->
     <div class="carousel-inner">
         <div class="carousel-item active">
@@ -65,10 +27,6 @@
         </div>
     </div>
 
-    <style> .frase{ font-family: 'Edu TAS Beginner', cursive;}
-
-        </style>
-
     <!-- Controles del carrusel -->
     <a class="carousel-control-prev" href="#promocionesCarousel" role="button" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -80,36 +38,16 @@
     </a>
 </div>
 
-
  <!-- Sección de Clientes -->
- <div class="container">
-    <h1 class="text-center mt-5">CLIENTES</h1>
-    <div id="clientesCarousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://unionandina.com/wp-content/uploads/2022/02/Ruta-PI-logo-1.png" class="d-block mx-auto" alt="Cliente 1">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.staffcreativa.pe/blog/wp-content/uploads/6_logotipos_peruanos_m%C3%A1s_recordados.jpg" class="d-block mx-auto" alt="Cliente 2">
-            </div>
-            <div class="carousel-item">
-                <img src="https://logos-marcas.com/wp-content/uploads/2021/12/Postobon-Logo.jpg" class="d-block mx-auto" alt="Cliente 3">
-            </div>
-            <!-- Agrega más imágenes de clientes según sea necesario -->
+ <div class="container clientes-section">
+    <h1>CLIENTES</h1>
+    <div class="clientes-container">
+        <div class="clientes-logos">
+            <img src="https://logos-marcas.com/wp-content/uploads/2021/12/Postobon-Logo.jpg" class="cliente-logo" alt="Cliente 1">
+            <img src="https://cdn.designcrowd.com/blog/2016/November/entrepreneur-logos/Logos-25-300x300%20luiz%20otavio%20I%20DESIGN.png" class="cliente-logo" alt="Cliente 2">
+            <img src="" class="cliente-logo" alt="Cliente 3">
+
         </div>
-        <a class="carousel-control-prev" href="#clientesCarousel" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Anterior</span>
-        </a>
-        <a class="carousel-control-next" href="#clientesCarousel" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Siguiente</span>
-        </a>
     </div>
 </div>
-
-
-
-
-
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,21 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Desarrollo Web Master</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Edu+TAS+Beginner&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+TAS+Beginner&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <!-- CSS de Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('css/index.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    <style>
-          li{
-        font-family: 'Montserrat', sans-serif;
-    }
-    </style>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
@@ -51,26 +48,11 @@
 @yield('content')
 
 
-
-
-
-
-
     <footer class="bg-dark text-light text-center py-3">
         <div class="container">
             <p>&copy; 2023 Desarrollo Web Master. Todos los derechos reservados.</p>
         </div>
     </footer>
-
-
-
-
-
-
-
-
-
-
 
 </body>
 </html>
